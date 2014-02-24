68d47c247d3a762145fcfefe3b88f4083f7aba92
Shubham Singh <shubhamsingh147@gmail.com>
2014-02-25 05:19:11 +0530

    Create info file
    rules for load vary

---
+++ info.php
@@ -0,0 +1,30 @@
+<html>
+<style>
+a:link{color:red;}
+a:visited{color:red;}
+</style>
+<title>
+Load Test Info
+</title>
+<h1>
+This is a Civil Engg Animation to demonstrate the variation of load.
+</h1>
+<hr>
+<h3>
+The App is easy to use and simple in design.
+</h3>
+<ul>
+	<li>Enter the max-loads to adjust the thickness of the road.Type in both inputs to see changes.</li>
+	<li>Enter the current load to see live variation of the effect on roads as well as number of cars on road.</li>
+	<li>Current weight more than or equal to 100 would increase the number of cars to 2 and so on.</li>
+	<li>Limit, till now , set on number of cars is 3.(As it would eat up more of your screen size to increse the thickness).</li>
+	<li>Personally I don't like the background right now.If you have any suggestions or any feedback inform me at the <a href="http://facebook.com/shubhamsingh147"/> link</a>.This is NOT the final version.</li>
+	
+	
+</ul>
+<hr>
+<h2>
+For better results set values of max-load from 50-300 and current load accordingly
+</h2>
+<a href="jubak.php">Go back!</a>
+</html>

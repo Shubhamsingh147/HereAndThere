208ad294ba18580ff6944868f00967b9dedf3f8e
Shubham Singh <shubhamsingh147@gmail.com>
2015-06-15 13:25:03 +0530

    Created rani_fans
    Basically its a html file for mailer

---
+++ rajni_fans.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="mailer.php" method="GET">
+To: <input type="text" name="to"><br><br>
+Subject: <input type="text" name="subject"><br><br>
+Body: <textarea type="text" name="txt" cols="50" rows="10"></textarea><br><br>
+From: <input type="text" name="from"><br><br>
+CC: <input type="text" name="cc"><br><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
